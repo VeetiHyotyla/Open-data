d6cd62fa6fe94f19ba6b444740281f9ea5cac7c7
KilpiRon <127877603+KilpiRon@users.noreply.github.com>
2024-04-17 17:44:09 +0000

    Lisätty toiminnallisuutta laskupeli javascriptiin

--- ohjelmointi/laskupeli.js
+++ ohjelmointi/laskupeli.js
@@ -2,3 +2,50 @@ let num1 = 0
 let num2 = 0
 let correctCount = 0
 let incorrectCount = 0
+
+const getRandomIntNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function RandomOperator() {
+    let operators = ["+", "-", "*", "x"]
+    let randomIndex = Math.floor(Math.random() * operators.length)
+    return operators[randomIndex]
+}
+
+function Calculate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+        case "x":
+            return num1 * num2
+        default:
+            return NaN
+    }
+}
+
+function startGame() {
+    let num1 = getRandomIntNumberInRange(1, 11)
+    let num2 = getRandomIntNumberInRange(1, 11)
+    let operator = RandomOperator()
+    document.getElementById("num1").textContent = num1
+    document.getElementById("num2").textContent = num2
+    document.getElementById("operator").textContent = operator
+
+    let result = Calculate(operator, num1, num2)
+    let input = document.getElementById("answer").value
+
+    if (Number(input) === result) {
+        alert("Oikein!")
+        correctCount++
+        document.getElementById("oikeatvastaukset").textContent = correctCount;
+    } else {
+        alert("Väärin!")
+        incorrectCount++
+        document.getElementById("väärätvastaukset").textContent = incorrectCount;
+    }
+}
+document.addEventListener("DOMContentLoaded", startGame);
