570ad20a6e8154e462e62c03c534e847109a7e07
VeetiHyotyla <n3hyve00@students.oamk.fi>
2024-04-23 13:36:46 +0300

    Lisätty kysymykset tehtävään

---
+++ ohjelmointi/monivalintapeli.js
@@ -0,0 +1,27 @@
+const Peruskysymykset = [ 
+    {
+      question: "Mikä on 3 + 5?", 
+      options: ["7", "8", "6", "9"], 
+      correctAnswerIndex: 1, 
+    }, 
+    { 
+      question: "Mikä on 12 - 4?", 
+      options: ["6", "7", "8", "9"], 
+      correctAnswerIndex: 2, 
+    }, 
+    { 
+      question: "Mikä on 7 * 3?", 
+      options: ["21", "24", "18", "27"], 
+      correctAnswerIndex: 0, 
+    }, 
+    { 
+      question: "Mikä on 20 / 4?", 
+      options: ["4", "6", "5", "7"], 
+      correctAnswerIndex: 2, 
+    }, 
+    { 
+      question: "Mikä on 2 + 2 * 3?", 
+      options: ["8", "6", "10", "9"], 
+      correctAnswerIndex: 0, 
+    }, 
+  ]; 
